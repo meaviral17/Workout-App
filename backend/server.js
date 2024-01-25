2c206eab8388890a857e3df2a10ade539feeba9c
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-01-26 01:12:38 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const workoutRoutes= require('./routes/workouts')
 require('dotenv').config()
 // express app
 const app = express()
 
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg:'Welcome to the app'})
+//middleware
+app.use(express.json())
+
+app.use((req,res,next) => {
+    console.log(req.path, req.method)
+    next()
 })
+// routes
+app.use('/api/workouts', workoutRoutes)
+
 
 // listen for requests
 app.listen(process.env.PORT, () =>  {
