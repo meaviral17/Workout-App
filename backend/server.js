ed2301b85003ea16d0659bc77ac6ca4d9c2c514d
Aviral Srivastava <81246801+meaviral17@users.noreply.github.com>
2024-01-25 21:38:18 +0530

    day1

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+// express app
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg:'Welcome to the app'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () =>  {
+    console.log('listening on port',process.env.PORT)
+})
